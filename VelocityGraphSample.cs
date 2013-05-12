94d58687a1d84d5bfd214154de76c7deed31cb89
Mats Persson <mats.persson@gmail.com>
2013-05-11 21:08:00 -0500

    Version 0.3

--- VelocityGraphSample.cs
+++ VelocityGraphSample.cs
@@ -9,12 +9,18 @@ using System.IO;
 using System.Linq;
 using VelocityDb.Session;
 using VelocityGraph;
+using Element = System.Int64;
+using PropertyTypeId = System.Int32;
+using PropertyId = System.Int32;
+using TypeId = System.Int32;
 
 namespace VelocityGraphSample
 {
   class VelocityGraphSample
   {
-    static readonly string systemDir = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "VelocityDB", "Databases", "VelocityGraphSample");
+    static readonly string systemDir = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments),
+      "VelocityDB" + Path.DirectorySeparatorChar + "Databases" + Path.DirectorySeparatorChar + "VelocityGraphSample");
+
     static void Main(string[] args)
     {
       if (Directory.Exists(systemDir))
@@ -27,69 +33,69 @@ namespace VelocityGraphSample
 
     // SCHEMA
         // Add a node type for the movies, with a unique identifier and two indexed Propertys
-        int movieType = g.NewNodeType("MOVIE");
-        int movieIdType = g.NewProperty(movieType, "ID", DataType.Long, PropertyKind.Unique);
-        int movieTitleType = g.NewProperty(movieType, "TITLE", DataType.String, PropertyKind.Indexed);
-        int movieYearType = g.NewProperty(movieType, "YEAR", DataType.Integer, PropertyKind.Indexed);
+        TypeId movieType = g.NewNodeType("MOVIE");
+        TypeId movieIdType = g.NewProperty(movieType, "ID", DataType.Long, PropertyKind.Unique);
+        PropertyId movieTitleType = g.NewProperty(movieType, "TITLE", DataType.String, PropertyKind.Indexed);
+        PropertyId movieYearType = g.NewProperty(movieType, "YEAR", DataType.Integer, PropertyKind.Indexed);
 
         // Add a node type for the people, with a unique identifier and an indexed Property
-        int peopleType = g.NewNodeType("PEOPLE");
-        int peopleIdType = g.NewProperty(peopleType, "ID", DataType.Long, PropertyKind.Unique);
-        int peopleNameType = g.NewProperty(peopleType, "NAME", DataType.String, PropertyKind.Indexed);
+        TypeId peopleType = g.NewNodeType("PEOPLE");
+        PropertyId peopleIdType = g.NewProperty(peopleType, "ID", DataType.Long, PropertyKind.Unique);
+        PropertyId peopleNameType = g.NewProperty(peopleType, "NAME", DataType.String, PropertyKind.Indexed);
 
         // Add an undirected edge type with an Property for the cast of a movie
-        int castType = g.NewEdgeType("CAST", false, false);
-        int castCharacterType = g.NewProperty(castType, "CHARACTER", DataType.String, PropertyKind.Basic);
+        TypeId castType = g.NewEdgeType("CAST", false, false);
+        PropertyId castCharacterType = g.NewProperty(castType, "CHARACTER", DataType.String, PropertyKind.Basic);
 
         // Add a directed edge type restricted to go from people to movie for the director of a movie
-        int directsType = g.NewRestrictedEdgeType("DIRECTS", peopleType, movieType, false);
+        TypeId directsType = g.NewRestrictedEdgeType("DIRECTS", peopleType, movieType, false);
 
-        // DATA
+    // DATA
         // Add some MOVIE nodes
 
-        long mLostInTranslation = g.NewNode(movieType);
+        Element mLostInTranslation = g.NewNode(movieType);
         g.SetProperty(mLostInTranslation, movieIdType, (long) 1);
         g.SetProperty(mLostInTranslation, movieTitleType, "Lost in Translation");
         g.SetProperty(mLostInTranslation, movieYearType, (int) 2003);
 
-        long mVickyCB = g.NewNode(movieType);
+        Element mVickyCB = g.NewNode(movieType);
         g.SetProperty(mVickyCB, movieIdType, (long) 2);
         g.SetProperty(mVickyCB, movieTitleType, "Vicky Cristina Barcelona");
         g.SetProperty(mVickyCB, movieYearType, (int) 2008);
 
-        long mManhattan = g.NewNode(movieType);
+        Element mManhattan = g.NewNode(movieType);
         g.SetProperty(mManhattan, movieIdType, (long) 3);
         g.SetProperty(mManhattan, movieTitleType, "Manhattan");
         g.SetProperty(mManhattan, movieYearType, (int) 1979);
 
 
         // Add some PEOPLE nodes
-        long pScarlett = g.NewNode(peopleType);
+        Element pScarlett = g.NewNode(peopleType);
         g.SetProperty(pScarlett, peopleIdType, (long) 1);
         g.SetProperty(pScarlett, peopleNameType, "Scarlett Johansson");
 
-        long pBill = g.NewNode(peopleType);
+        Element pBill = g.NewNode(peopleType);
         g.SetProperty(pBill, peopleIdType, (long) 2);
         g.SetProperty(pBill, peopleNameType, "Bill Murray");
 
-        long pSofia = g.NewNode(peopleType);
+        Element pSofia = g.NewNode(peopleType);
         g.SetProperty(pSofia, peopleIdType, (long) 3);
         g.SetProperty(pSofia, peopleNameType, "Sofia Coppola");
 
-        long pWoody = g.NewNode(peopleType);
+        Element pWoody = g.NewNode(peopleType);
         g.SetProperty(pWoody, peopleIdType, (long) 4);
         g.SetProperty(pWoody, peopleNameType, "Woody Allen");
 
-        long pPenelope = g.NewNode(peopleType);
+        Element pPenelope = g.NewNode(peopleType);
         g.SetProperty(pPenelope, peopleIdType, (long) 5);
         g.SetProperty(pPenelope, peopleNameType, "Penélope Cruz");
 
-        long pDiane = g.NewNode(peopleType);
+        Element pDiane = g.NewNode(peopleType);
         g.SetProperty(pDiane, peopleIdType, (long) 6);
         g.SetProperty(pDiane, peopleNameType, "Diane Keaton");
 
         // Add some CAST edges
-        long anEdge;
+        Element anEdge;
         anEdge = g.NewEdge(castType, mLostInTranslation, pScarlett);
         g.SetProperty(anEdge, castCharacterType, "Charlotte");
 
@@ -115,23 +121,23 @@ namespace VelocityGraphSample
 
     // QUERIES
         // Get the movies directed by Woody Allen
-        Objects directedByWoody = g.Neighbors(pWoody, directsType, EdgesDirection.Outgoing);
+        Elements directedByWoody = g.Neighbors(pWoody, directsType, EdgesDirection.Outgoing);
 
         // Get the cast of the movies directed by Woody Allen
-        Objects castDirectedByWoody = g.Neighbors(directedByWoody, castType, EdgesDirection.Any);
+        Elements castDirectedByWoody = g.Neighbors(directedByWoody, castType, EdgesDirection.Any);
 
 
         // Get the movies directed by Sofia Coppola
-        Objects directedBySofia = g.Neighbors(pSofia, directsType, EdgesDirection.Outgoing);
+        Elements directedBySofia = g.Neighbors(pSofia, directsType, EdgesDirection.Outgoing);
 
         // Get the cast of the movies directed by Sofia Coppola
-        Objects castDirectedBySofia = g.Neighbors(directedBySofia, castType, EdgesDirection.Any);
+        Elements castDirectedBySofia = g.Neighbors(directedBySofia, castType, EdgesDirection.Any);
 
         // We want to know the people that acted in movies directed by Woody AND in movies directed by Sofia.
-        Objects castFromBoth = Objects.CombineIntersection(castDirectedByWoody, castDirectedBySofia);
+        Elements castFromBoth = Elements.CombineIntersection(castDirectedByWoody, castDirectedBySofia);
 
         // Say hello to the people found
-        foreach (long peopleOid in castFromBoth)
+        foreach (Element peopleOid in castFromBoth)
         {
           object value = g.GetProperty(peopleOid, peopleNameType);
           System.Console.WriteLine("Hello " + value);
@@ -144,6 +150,9 @@ namespace VelocityGraphSample
       {
         session.BeginRead();
         Graph g = (Graph) session.Open(graphId);
+        TypeId movieType = g.FindType("MOVIE");
+        PropertyId movieTitleProperty = g.FindProperty(movieType, "TITLE");
+        Element obj = g.FindElement(movieTitleProperty, "Manhattan");
         session.Commit();
       }
     }
